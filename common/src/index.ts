308f7a3e5a9831ad4a0968730e2bc3497dab407d
abhijeet141 <sinhaabhijeet.smith@gmail.com>
2024-09-27 23:09:37 +0530

    zod-common

---
+++ common/src/index.ts
@@ -0,0 +1,32 @@
+import z from "zod"
+
+export const signUpInput = z.object({
+    email: z.string().email(),
+    password: z.string().min(5),
+    name: z.string()
+})
+
+export type SignUpInput = z.infer<typeof signUpInput>
+
+export const signInInput = z.object({
+    email: z.string().email(),
+    password: z.string().min(5),
+})
+
+export type SignInInput = z.infer<typeof signInInput>
+
+export const blogInput = z.object({
+    title: z.string(),
+    content: z.string()
+})
+
+export type BlogInput = z.infer<typeof blogInput>
+
+export const updateblogInput = z.object({
+    title: z.string(),
+    content: z.string(),
+    id: z.string()
+})
+
+export type updateBlogInput = z.infer<typeof updateblogInput>
+
