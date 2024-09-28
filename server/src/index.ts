d96ee8591175103f0ee898b6b6030103b1b0e1af
abhijeet141 <sinhaabhijeet.smith@gmail.com>
2024-09-28 23:05:00 +0530

    cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,12 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+
 
 import router from './routes'
 
 const app = new Hono()
+
+app.use('/api/v1/*', cors())
 
 app.route('/api/v1',router)
 
