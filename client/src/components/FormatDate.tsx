7c995ea3385c8bb68bd3cf402699ba97a2900d95
abhijeet141 <sinhaabhijeet.smith@gmail.com>
2024-09-30 03:13:21 +0530

    Date Format

---
+++ client/src/components/FormatDate.tsx
@@ -0,0 +1,8 @@
+export function FormatDate(Inputdate: string):string{
+    const date = new Date(Inputdate);
+    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    const month = monthNames[date.getMonth()];
+    const day = date.getDate();
+    const year = date.getFullYear();
+    return `${month} ${day}, ${year}`;
+}
