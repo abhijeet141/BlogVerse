7aa784a26810c25d98c36c4f18743cd02d29deda
abhijeet141 <sinhaabhijeet.smith@gmail.com>
2024-09-30 03:16:23 +0530

    Blog Page

---
+++ client/src/pages/AuthorPost.tsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from "react";
+import { SkeletonDemo } from "@/components/SkeletonComponent";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { BlogCard } from "@/components/BlogCard";
+import { BlogInput } from "@abhi209/blogverse-common";
+
+export function AuthorPost(){
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+    const [blogs,setBlogs] = useState([])
+    const [loading,setLoading] = useState(true)
+    const {id} = useParams();
+
+    useEffect(()=>{
+        const getAuthorBlogs = async() =>{
+            try{
+                const response = await axios.get(`${backendUrl}/api/v1/blog/author/${id}`,{
+                    headers:{
+                        Authorization: "Bearer " + localStorage.getItem('tokenId')
+                    }
+                })
+                setBlogs(response.data.blog)                
+            }
+            catch(error){
+                console.error("Error Fetching Author Blogs")
+            }
+            finally{
+                setLoading(false)
+            }
+        }
+        getAuthorBlogs();
+    },[id])
+
+    if(loading){
+        return (
+            <div className="container">
+            <SkeletonDemo></SkeletonDemo>
+            <SkeletonDemo></SkeletonDemo>
+            <SkeletonDemo></SkeletonDemo>
+            </div>
+        )
+    }
+
+
+    return (
+        <div className="container">
+            {blogs.map((blog: BlogInput,index)=>(
+                 <BlogCard key = {index}
+                           authorName={blog.author.name}
+                           id = {blog.id}
+                           title = {blog.title}
+                           content={blog.content}
+                           publishedDate = {blog.publishedDate}
+                           authorId={blog.authorId}
+                           />
+            ))}
+        </div>
+    )
+}
---
+++ client/src/pages/BlogPost.tsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { SkeletonPostDemo } from "@/components/SkeletonPostDemo";
+import { FormatDate } from "@/components/FormatDate";
+import { CalculateReadingTime } from "@/components/CalculateReadingTime";
+
+interface BlogProps{
+    author: {name: string},
+    title: string,
+    content: string,
+    publishedDate: string 
+}
+
+export function BlogPost() {
+  const [blog, setBlog] = useState<BlogProps | null>(null); 
+  const [loading, setLoading] = useState(true);
+  const { id } = useParams();
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  useEffect(() => {
+    const getBlogDetails = async () => {
+      try {
+        const response = await axios.get(`${backendUrl}/api/v1/blog/${id}`, {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("tokenId"),
+          },
+        });
+        setBlog(response.data.blog);
+        console.log(response.data.blog);
+        
+      } catch (error) {
+        console.error("Error Fetching Blog Details");
+      } finally {
+        setLoading(false);
+      }
+    };
+    getBlogDetails();
+  }, [id]);
+
+
+  if (loading) {
+    return <div className="container"><SkeletonPostDemo></SkeletonPostDemo></div>;
+  }
+
+  if (!blog) {
+    return <div className="container">No blog found</div>;
+  }
+
+  return (
+    <div className="container flex flex-col">
+      <div className="text-3xl font-bold py-6">{blog.title}</div>
+      <div>
+      <div className="cursor-pointer hover:underline underline-offset-2">
+        {blog.author.name}
+      </div>
+      <div className="flex py-6">
+        <div>
+        {`${CalculateReadingTime(blog.content)} min read`}
+        </div>
+        <div className="pl-4">
+            {FormatDate(blog.publishedDate)}
+        </div>
+      </div>
+      </div>
+      {/* <div className="py-6">Blog Image</div> */}
+      <div className="text-lg text-slate-700 text-justify">{blog.content}</div>
+    </div>
+  );
+}
